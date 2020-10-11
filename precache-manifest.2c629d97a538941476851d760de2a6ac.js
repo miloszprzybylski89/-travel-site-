31c494927d84a403e15fe969263cca36f010b096
Miłosz Przybylski <miloszprzybylski89@gmail.com>
2020-10-11 14:17:37 +0200

    Updates

---
+++ precache-manifest.2c629d97a538941476851d760de2a6ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245b23b58143577b0018b1687afcb90d",
+    "url": "/travel-site/index.html"
+  },
+  {
+    "revision": "89d10e623c1a011b9516",
+    "url": "/travel-site/static/css/main.88ba8fd7.chunk.css"
+  },
+  {
+    "revision": "2fae7d8d48219b0d3a47",
+    "url": "/travel-site/static/js/2.5d0bf25c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travel-site/static/js/2.5d0bf25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d10e623c1a011b9516",
+    "url": "/travel-site/static/js/main.71e88131.chunk.js"
+  },
+  {
+    "revision": "cff7949b1a3b5fd87e34",
+    "url": "/travel-site/static/js/runtime-main.47103047.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/travel-site/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/travel-site/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/travel-site/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/travel-site/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/travel-site/static/media/img-8.d211c298.jpg"
+  },
+  {
+    "revision": "523a89f64e53deb7aa02e9ff0fa6d18f",
+    "url": "/travel-site/static/media/img-9.523a89f6.jpg"
+  }
+]);
